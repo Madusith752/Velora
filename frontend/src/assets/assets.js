cc9499151f418c93b818b8903bc10fc1d15dcdec
Madusith752 <madusithdissanayake77@gmail.com>
2026-02-10 11:50:32 +0530

    initialTest1

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,140 @@
+import bin_icon from './bin_icon.webp'
+import cart_icon from './cart_icon.png'
+import cross_icon from './cross_icon.webp'
+import exchange_icon from './exchange_icon.webp'
+import k1_1 from '/k1_1.png'
+import k1_2 from './k1_2.png'
+import k1_3 from './k1_3.png'
+import k1_4 from './k1_4.png'
+import k2_1 from './k2_1.png'
+import k2_2 from './k2_2.png'
+import k2_3 from './k2_3.png'
+import k2_4 from './k2_4.png'
+import k3_1 from './k3_1.png'
+import k3_2 from './k3_2.png'
+import k3_3 from './k3_3.png'
+import k3_4 from './k3_4.png'
+import k4_1 from './k4_1.png'
+import k4_2 from './k4_2.png'
+import k4_3 from './k4_3.png'
+import k4_4 from './k4_4.png'
+import k5_1 from './k5_1.png'
+import k5_2 from './k5_2.png'
+import k5_3 from './k5_3.png'
+import k5_4 from './k5_4.png'
+import k6_1 from './k6_1.png'
+import k6_2 from './k6_2.png'
+import k6_3 from './k6_3.png'
+import k6_4 from './k6_4.png'
+import k7_1 from './k7_1.png'
+import k7_2 from './k7_2.png'
+import k7_3 from './k7_3.png'
+import k7_4 from './k7_4.png'
+import k8_1 from './k8_1.png'
+import k8_2 from './k8_2.png'
+import k8_3 from './k8_3.png'
+import k8_4 from './k8_4.png'
+
+import Logo from'./Logo.png'
+
+import m1_1 from './m1_1.png'
+import m1_2 from './m1_2.png'
+import m1_3 from './m1_3.png'
+import m1_4 from './m1_4.png'
+import m2_1 from './m2_1.png'
+import m2_2 from './m2_2.png'
+import m2_3 from './m2_3.png'
+import m2_4 from './m2_4.png'
+import m3_1 from './m3_1.png'
+import m3_2 from './m3_2.png'
+import m3_3 from './m3_3.png'
+import m3_4 from './m3_4.png'
+import m4_1 from './m4_1.png'
+import m4_2 from './m4_2.png'
+import m4_3 from './m4_3.png'
+import m4_4 from './m4_4.png'
+import m5_1 from './m5_1.png'
+import m5_2 from './m5_2.png'
+import m5_3 from './m5_3.png'
+import m5_4 from './m5_4.png'
+import m6_1 from './m6_1.png'
+import m6_2 from './m6_2.png'
+import m6_3 from './m6_3.png'
+import m6_4 from './m6_4.png'
+import m7_1 from './m7_1.png'
+import m7_2 from './m7_2.png'
+import m7_3 from './m7_3.png'
+import m7_4 from './m7_4.png'
+import m8_1 from './m8_1.png'
+import m8_2 from './m8_2.png'
+import m8_3 from './m8_3.png'
+import m8_4 from './m8_4.png'
+import m9_1 from './m9_1.png'
+import m9_2 from './m9_2.png'
+import m9_3 from './m9_3.png'
+import m9_4 from './m9_4.png'
+import m10_1 from './m10_1.png'
+import m10_2 from './m10_2.png'
+import m10_3 from './m10_3.png'
+import m10_4 from './m10_4.png'
+
+import w1_1 from './w1_1.png'
+import w1_2 from './w1_2.png'
+import w1_3 from './w1_3.png'
+import w1_4 from './w1_4.png'
+import w2_1 from './w2_1.png'
+import w2_2 from './w2_2.png'
+import w2_3 from './w2_3.png'
+import w2_4 from './w2_4.png'
+import w3_1 from './w3_1.png'
+import w3_2 from './w3_2.png'
+import w3_3 from './w3_3.png'
+import w3_4 from './w3_4.png'
+import w4_1 from './w4_1.png'
+import w4_2 from './w4_2.png'
+import w4_3 from './w4_3.png'
+import w4_4 from './w4_4.png'
+import w5_1 from './w5_1.png'
+import w5_2 from './w5_2.png'
+import w5_3 from './w5_3.png'
+import w5_4 from './w5_4.png'
+import w6_1 from './w6_1.png'
+import w6_2 from './w6_2.png'
+import w6_3 from './w6_3.png'
+import w6_4 from './w6_4.png'
+
+export const assets = {
+    Logo,
+    bin_icon,
+    cart_icon,
+    cross_icon,
+    exchange_icon
+}
+
+
+export const products = [
+    {
+        _id:"aaaaa",
+        name: "Women Round Neck Cotton Top",
+        description: "Blubelle Women's winter Thick Cashmere Knitted 2-Piece set- Soft Polo Neck Sweater & Wide Leg Pants Outfit",
+        price: 30,
+        image: [w1_1],
+        category: "Women",
+        subCategory: "WholeWear",
+        sizes: ["S","M","L"],
+        date:1716634345448,
+        bestseller: true
+    },
+    {
+        _id:"aaaab",
+        name: "Men's Classic Plaid Flanne; Shirt",
+        description: "this shirt features a soft, durable fabric that keeps you warm without feeling heavy.",
+        price:20,
+        image: [m1_1,m1_2,m1_3,m1_4],
+        category: "men",
+        subCategory: "TopWear",
+        sizes: ["S","M","L"],
+        date:1716634345448,
+        bestseller: true
+    }
+]
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+
+        <img src="/logo.png" alt="Logo" className='w-16 h-16 object-contain'/>
+      
+    </div>
+  )
+}
+
+export default Navbar
